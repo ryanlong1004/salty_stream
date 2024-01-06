836fc9ae490ad4d681d00fe124d5731aca033963
Ryan Long <ryan.long@noaa.gov>
2024-01-05 23:26:23 -0500

    Ch5 and 6

---
+++ ch5.py
@@ -0,0 +1,5 @@
+eggs = {
+    'names': ['Ryan', 'Dom', 'Tanis'], 
+    'species': {'calico': 'black white red'}, 
+    'ages': 8}
+
---
+++ ch6.py
@@ -0,0 +1,9 @@
+spam = 'That is Alice\'s cat.'
+
+def print_spam():
+    """will print spam
+    """
+    name = "spamarific"
+    print(f"{name} is the best thing mom serves")
+    
+print_spam()
